aea010717ed71e3a46aea06e5aedbfe69dc2a7ce
Jason Park <jjpark987@gmail.com>
2022-10-19 12:09:32 -0700

    commit tree 1b8e4038f18d73fe8f278342403c22bb91478401

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(arrayOfStringNames, eventName) {
+    let newArray = [];
+
+    for(let i = 0; i < arrayOfStringNames.length; i++) {
+        newArray.push(`Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`);    
+    }
+
+    return newArray;
+}
+
+function countDown(integer) {
+    let i = integer;
+    
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
